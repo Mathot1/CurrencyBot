120966adf35b5f6a26443de873a43239f929a05a
Mathot1 <mateuszblige112@gmail.com>
2019-02-10 13:59:40 +0100

    yes

---
+++ commands/random/weekly.js
@@ -0,0 +1,53 @@
+const Discord = require('discord.js-commando');
+const money = require('discord-money');
+const moment = require('moment');
+const ms = require('parse-ms');
+let cooldown = new Set();
+let cdseconds = 58800;
+global.coldown;
+class WeeklyCommand extends Discord.Command {
+
+  constructor(client){
+    super(client, {
+      name: 'daily',
+      group: 'random',
+      memberName: 'daily',
+      description: '**ADMIN ONLY** Use it to receive daily amount of money'
+    })
+
+  }
+
+  async run(message, args){
+    let autor = "<@" + message.author.id + ">";
+    let adm = message.guild.roles.find("name", "Administrator");
+    if (!message.member.roles.has(adm.id)) {
+
+    }
+    else {
+              if (money[message.author.username + message.guild.name] != moment().format('L')) {
+                  money[message.author.username + message.guild.name] = moment().format('L')
+                  money.updateBal(autor, 20).then((i) => { // The daily ends of the day, so everyday they can get a daily bonus, if they missed it, they can't get it back again.
+                      message.channel.send({embed: {
+                          color: 3447003,
+                          description: 'Recieved your **20** \`!weekly`\. Check \`!balance\`.',
+                          author: {
+                              name: `${message.author.username}#${message.author.discriminator}`,
+                              icon_url: message.author.avatarURL
+                          }
+                      }});
+                  })
+              } else {
+                  message.channel.send({embed: {
+                      color: 3447003,
+                      description: 'You already recieved your \`!weekly`\. Check later **' + moment().endOf('day').fromNow() + '**.', // When you got your daily already, this message will show up.
+                      author: {
+                          name: `${message.author.username}#${message.author.discriminator}`,
+                          icon_url: message.author.avatarURL
+                      }
+                  }});
+              }
+
+}
+}
+}
+  module.exports = WeeklyCommand;
---
+++ index.js
@@ -0,0 +1,41 @@
+
+
+
+    const Discord = require('discord.js-commando');
+    const money = require('discord-money');
+    const moment = require('moment');
+    // Define client for Discord
+    const client = new Discord.Client();
+
+    client.registry.registerGroup('random', 'AssemblyCoin');
+    client.registry.registerDefaults();
+    client.registry.registerCommandsIn(__dirname + "/commands");
+      // This runs when a message is recieved...
+let prefix = '!';
+
+        client.on("guildMemberAdd", function(member) {
+
+          member.send("**Welcome to the Assemblyline server!**");
+          let memberRole = member.guild.roles.find("name", "public");
+          member.addRole(memberRole);
+        });
+
+
+
+    client.on('message', message => {
+
+        // Prefix
+//a
+
+
+        // Example: Getting a daily reward
+
+
+
+    });
+
+
+
+
+
+client.login(process.env.BOT_TOKEN);
