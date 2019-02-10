20bf6d6e4efb84869796f6d370f5b37c9a7c0685
Mathot1 <mateuszblige112@gmail.com>
2019-02-10 15:47:03 +0100

    a

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 
 
 
-    const Discord = require('discord.js');
-    //const money = require('discord-money');
-    //const moment = require('moment');
+    const Discord = require('discord.js-commando');
+    const money = require('discord-money');
+    const moment = require('moment');
     // Define client for Discord
     const client = new Discord.Client();
 
@@ -15,7 +15,7 @@ let prefix = '!';
 
         client.on("guildMemberAdd", function(member) {
 
-          member.send("**Welcome to the Assemblyline server!**\n I am assemblybot, type !help for commands");
+          member.send("**Welcome to the Assemblyline server!**");
           let memberRole = member.guild.roles.find("name", "public");
           member.addRole(memberRole);
         });
@@ -25,11 +25,11 @@ let prefix = '!';
     client.on('message', message => {
 
         // Prefix
-//a
+
 
 
         // Example: Getting a daily reward
-
+        
 
 
     });
@@ -38,4 +38,4 @@ let prefix = '!';
 
 
 
-client.login(process.env.BOT_TOKEN);
+client.login('NTM4NjQ5NzkwMDgzMzY2OTEy.DzWwHA.YOqZAs2byDBxt2d483srT7NqFTU');
