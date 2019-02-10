70d9e5d03f68969a25166f716e2d6c0529e81d08
Mathot1 <mateuszblige112@gmail.com>
2019-02-10 16:16:15 +0100

    j

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 
 
 
-    const Discord = require('discord.js-commando');
+    const Discord = require('discord.js');
     //const money = require('discord-money');
     //const moment = require('moment');
     // Define client for Discord
     const client = new Discord.Client();
 
-    client.registry.registerGroup('random', 'AssemblyCoin');
-    client.registry.registerDefaults();
-    client.registry.registerCommandsIn(__dirname + "/commands");
+    //client.registry.registerGroup('random', 'AssemblyCoin');
+    //client.registry.registerDefaults();
+    //client.registry.registerCommandsIn(__dirname + "/commands");
       // This runs when a message is recieved...
 let prefix = '!';
 
